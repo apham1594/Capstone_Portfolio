141ab5022093ec7bcc0bd7f491303ed9e9d3fcda
apham1594 <apham1594@gmail.com>
2022-07-16 10:45:21 -0600

    made changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    strings:["","Frontend Developer","Web Designer","Graphic Designer","UX Designer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
